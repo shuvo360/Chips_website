610a983b6f0bca7dc8d964d813bba68c70f68263
shuvo360 <k.shuvo360@gmail.com>
2024-08-25 22:45:43 +0600

    final

--- main.js
+++ main.js
@@ -44,7 +44,6 @@ const swiperFavorites = new Swiper(".favorites__swiper", {
   grabCursor: true,
   slidesPerView: "auto",
   centeredSlides: "auto",
-
 });
 
 /*=============== SHOW SCROLL UP ===============*/
@@ -81,3 +80,23 @@ const scrollActive = () => {
 window.addEventListener("scroll", scrollActive);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 300,
+});
+sr.reveal(`.home__data, .favorites__container, .footer__container`);
+sr.reveal(`.home__circle, .home__img`, { delay: 600, scale: 0.5 });
+sr.reveal(`.home__chips-1, .home__chips-2, .home__chips-3`, {
+  delay: 1000,
+  interval: 100,
+});
+sr.reveal(`.home__leaf`, { delay: 1200 });
+sr.reveal(`.home__tomato-1, .home__tomato-2`, {
+  delay: 1400,
+  interval: 100,
+});
+sr.reveal(`.care__img, .contact__img`, { origin: "left" });
+sr.reveal(`.care__list, .contact__data`, { origin: "right" });
+sr.reveal(`.banner__item, .products__card`, { interval: 100 });
